4d6be22d95244c358a0777b54c9ae2618a07b685
Marinho1422 <dudimarinho2016@gmail.com>
2025-02-11 05:45:38 -0300

    Opção de saída
    incluso no sistema opção de saída para os usuários.

--- Cartório proj.cpp
+++ Cartório proj.cpp
@@ -132,6 +132,11 @@ int main ()
 	deletar();
 	break;
 	
+	case 4:
+	printf("Obrigado por utilizar o nosso sistema!\n");
+	return 0;
+	break;
+	
 	default:
 		printf("Essa opção não está disponível\n");
 		system("pause");
@@ -144,6 +149,7 @@ printf("Escolha a opção desejada do menu: \n\n");
 printf("\t1 Registrar nomes\n");
 printf("\t2 Consultar nomes\n");
 printf("\t3 Deletar nomes\n\n"); 
+printf("\t4 Sair do sistema\n\n"); 
 printf("Opção: ");//Fim do menu
 
 scanf("%d",&opcao); //Armazenando as escolha do usuário	
